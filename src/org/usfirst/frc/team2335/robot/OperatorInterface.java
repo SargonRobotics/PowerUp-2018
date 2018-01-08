e527b6325ce353b522d82469a24bcd2658cd42ff
Ubspy <jack-m@live.com>
2018-01-08 13:23:31 -0600

    Cleaned up code, removed unused files, skeleton code for drive

--- src/org/usfirst/frc/team2335/robot/OperatorInterface.java
+++ src/org/usfirst/frc/team2335/robot/OperatorInterface.java
@@ -1,17 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team2335.robot;
 
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
-public class OI {
+public class OperatorInterface
+{
+	//TODO: Remove this mess below
+	
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
---
+++ src/org/usfirst/frc/team2335/robot/subsystems/Drive.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2335.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drive extends Subsystem
+{
+	//TODO: Add motor controllers
+
+	//TODO: Add drive commands
+	
+    public void initDefaultCommand()
+    {
+     
+    }
+}
+
