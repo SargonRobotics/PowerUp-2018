4e60576a3db2b34e717fc03f7c4644f0e9e0551b
Ubspy <jack-m@live.com>
2018-01-13 11:09:51 -0600

    Changed to used constants

--- src/org/usfirst/frc/team2335/robot/subsystems/Ultrasound.java
+++ src/org/usfirst/frc/team2335/robot/subsystems/Ultrasound.java
@@ -1,15 +1,17 @@
 package org.usfirst.frc.team2335.robot.subsystems;
 
+import org.usfirst.frc.team2335.robot.Robot;
+
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Ultrasound extends Subsystem
-{
+{	
 	Ultrasonic sensor;
 	
 	public Ultrasound()
 	{
-		sensor = new Ultrasonic(1, 0);
+		sensor = new Ultrasonic(Robot.PULSE_PIN, Robot.ECHO_PIN);
 		
 		//TODO: Test automatic mode
 		sensor.setAutomaticMode(true);
