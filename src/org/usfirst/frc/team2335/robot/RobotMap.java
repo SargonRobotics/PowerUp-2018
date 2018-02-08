9a01dbef489688cec4a1aade9c5cd9bd0c3b7bf8
Ubspy <jack-m@live.com>
2018-02-08 10:52:39 -0600

    Fixed formatting

--- src/org/usfirst/frc/team2335/robot/RobotMap.java
+++ src/org/usfirst/frc/team2335/robot/RobotMap.java
@@ -2,21 +2,22 @@ package org.usfirst.frc.team2335.robot;
 
 public class RobotMap
 {
+	//Deadzone constant
 	public static final double deadzone = 0.15;
-	
-	//Motor controller constants
-	public static class MotorControllers
-	{
-		public static final int leftDrive = 0, rightDrive = 1;
-	}
 	
 	//Controller constants
 	public static class Controller
 	{
 		//Controller axes constants
-		static class Axes
+		public static class Axes
 		{
 			public static final int xDrive = 0, yDrive = 1;
 		}
+	}
+	
+	//Motor controller constants
+	public static class MotorControllers
+	{
+		public static final int leftDrive = 0, rightDrive = 1;
 	}
 }
